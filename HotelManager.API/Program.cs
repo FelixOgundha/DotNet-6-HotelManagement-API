dfd7e0f358ee4682c6a248ed7ecc5057a8829b33
FelixOgundha <fogundha@strathmore.edu>
2022-05-29 22:44:56 +0300

    Add project files.

---
+++ HotelManager.API/Program.cs
@@ -0,0 +1,60 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//1. Adding Serilog to log our app Data
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console()
+    .WriteTo.Seq("http://localhost:5341")
+    .ReadFrom.Configuration(ctx.Configuration));
+
+Log.Information("Hello Felix System is Starting up...");
+
+
+try
+{
+    // Add services to the container.
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    //Adding Cross Origin Resource Sharing
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("ÄllowAll", b => b.AllowAnyHeader().
+        AllowAnyOrigin().
+        AllowAnyMethod());
+    });
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    app.UseSerilogRequestLogging();
+
+    app.UseHttpsRedirection();
+
+    app.UseCors("AllowAll");
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
+
+
